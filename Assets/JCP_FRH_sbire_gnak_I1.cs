a276a8d85cde22d1978f4abef4c30f475a1f8b1b
Jango Course <jango.course@gmail.com>
2020-02-19 01:57:04 +0100

    - Managed to change the Active Sector of a Henchman to a different Sector by hard-referencing the new desired Sector by name. It correctly activates on game load, and thus also updates its lighting accordingly.

---
+++ Assets/JCP_FRH_sbire_gnak_I1.cs
@@ -0,0 +1,50 @@
+﻿using OpenSpace;
+using OpenSpace.Object;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JCP_FRH_sbire_gnak_I1 : MonoBehaviour {
+    public PersoBehaviour pirate;
+    public SectorComponent sector;
+
+    private int initSector = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pirate == null) {
+            foreach (PersoBehaviour pb in FindObjectsOfType<PersoBehaviour>()) {
+                string pbName = pb.perso.namePerso;
+
+                if (pbName == "JCP_FRH_sbire_gnak_I1") {
+                    //Debug.LogError(pbName);
+                    pirate = pb;
+                }
+            }
+        } else {
+            if (initSector == 0) {
+                //change the active Sector for this Henchman to the area with the climbing tree and the Caterpillar
+                foreach (SectorComponent sc in FindObjectsOfType<SectorComponent>()) {
+                    if (sc.name == "Sector @ Learn_31|0x000308F4, SPO @ Learn_31|0x000276F4") {
+                        sector = sc;
+                        break;
+                    }
+                }
+                if (!(sector == null)) {
+                    pirate.sector = sector;
+                }
+
+                //transform
+                pirate.transform.position = new Vector3(-185, 22, 356);
+                pirate.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+                initSector = 1;
+            }
+
+            else if (initSector == 1) {
+                //more stuff
+            }
+        }
+    }
+}
