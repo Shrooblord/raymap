a6f635fc5f9301ec1e868ea5ca38db777459a9c1
Jango Course <jango.course@gmail.com>
2020-02-20 19:06:17 +0100

    - Added rudimentary apex handle for jumpTo path types between Waypoints.

--- Assets/Scripts/Shrooblord/lib/SHR_Waypoint.cs
+++ Assets/Scripts/Shrooblord/lib/SHR_Waypoint.cs
@@ -27,7 +27,7 @@ public class SHR_Waypoint : MonoBehaviour {
     //public bool jumpFromHere = false;
     //[Tooltip("Define this Waypoint as one that a Perso could jump to from another one. The instigating WP needs to have this one as one of its connections, and have the jumpFromHere flag checked.")]
     //public bool jumpToHere = false;
-    private Transform jumpCurveHandle;
+    public Transform jumpCurveHandle;
 
     public float waitHere = 0.0f;       //when > 0, it causes the Perso to stop and wait on this WP for this many seconds
 
@@ -45,24 +45,84 @@ public class SHR_Waypoint : MonoBehaviour {
 
         var whiteCol = graph.graphColor;
 
-        foreach (var conn in prev) {
+        /*
+         foreach (var conn in prev) {
             if (conn?.wp != null) {
                 Gizmos.color = Color.blue;
                 Gizmos.DrawSphere(transform.position + (conn.wp.transform.position - transform.position).normalized + lineOffset, .4f);
             }
         }
+        */
 
         foreach (var conn in next) {
             if (conn != null) {
                 Gizmos.color = whiteCol;
-                Gizmos.DrawLine(transform.position + lineOffset, conn.wp.transform.position + lineOffset);
 
-                Gizmos.color = Color.green;
-                Gizmos.DrawSphere(transform.position + (conn.wp.transform.position - transform.position).normalized + lineOffset, .4f);
+                switch (conn.type) {
+                    case WPConnection.Type.JumpTo:
+                        var midPos = (conn.wp.transform.position + transform.position) / 2;
+
+                        if (conn.wp.jumpCurveHandle == null) {
+                            conn.wp.jumpCurveHandle = new GameObject("HDL_jumpCurve").transform;
+                            conn.wp.jumpCurveHandle.position = midPos + Vector3.up * 5;
+                        }
+
+                        var handle = conn.wp.jumpCurveHandle;
+                        
+                        handle.position = new Vector3(midPos.x, handle.position.y, midPos.z);
+                        handle.parent = transform;
+                        
+
+                        //var dir = conn.wp.transform.position - transform.position;
+                        //handle.position = new Vector3();
+
+                        /*
+                         * attempt at making it curved
+                         * 
+                        float steps = 10;
+                        for (float x = 0; x < 1.0f; x += (1.0f / steps)) {
+                            Vector3 diff = (conn.wp.transform.position - transform.position);
+
+                            Gizmos.DrawLine(
+                                transform.position + diff * x,
+                                transform.position + diff * (x * (1.0f / steps))
+                            );
+                        }
+                        */
+                        Gizmos.color = Color.green;
+                        Gizmos.DrawSphere(transform.position + (handle.position - transform.position).normalized + lineOffset, .4f);
+
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawSphere(conn.wp.transform.position + (handle.position - conn.wp.transform.position).normalized + lineOffset, .4f);
+
+                        Gizmos.color = whiteCol;
+                        Gizmos.DrawLine(transform.position + lineOffset, handle.position + lineOffset);
+                        Gizmos.DrawLine(handle.position + lineOffset, conn.wp.transform.position + lineOffset);
+
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawSphere(handle.position + lineOffset, 0.4f);
+                        Gizmos.color = whiteCol;
+
+                        break;
+                    default:
+                        if (conn.wp.jumpCurveHandle != null) {
+                            DestroyImmediate(conn.wp.jumpCurveHandle.gameObject);
+                        }
+                        
+                        Gizmos.DrawLine(transform.position + lineOffset, conn.wp.transform.position + lineOffset);
+
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawSphere(conn.wp.transform.position + (transform.position - conn.wp.transform.position).normalized + lineOffset, .4f);
+
+                        Gizmos.color = Color.green;
+                        Gizmos.DrawSphere(transform.position + (conn.wp.transform.position - transform.position).normalized + lineOffset, .4f);
+                        break;
+                }
             }
         }
 
         Gizmos.color = whiteCol;
         Gizmos.DrawSphere(transform.position + lineOffset, 1f);
+
     }
 }
