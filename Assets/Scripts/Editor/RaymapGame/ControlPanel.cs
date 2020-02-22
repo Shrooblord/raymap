325e12634a13b0a6b3c11c5a007cc1a4ed585e28
Adsolution9876 <rayfan9876@gmail.com>
2020-02-22 02:03:38 -0800

    Added Scripts/Editor directory to gitignore exception

---
+++ Assets/Scripts/Editor/RaymapGame/ControlPanel.cs
@@ -0,0 +1,102 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+namespace RaymapGame {
+    public class ControlPanel : EditorWindow {
+        [MenuItem("Raymap/RaymapGame/Control Panel")]
+        public static void ShowWindow() {
+            GetWindow(typeof(ControlPanel));
+        }
+
+        public static void CreateNewPersoScript(string persoNameOrModelOrFamily, string englishName, string author = "") {
+            string newPath = $"Assets/Scripts/RaymapGame/Rayman2/Persos/{persoNameOrModelOrFamily}.cs";
+            if (File.Exists(newPath)) {
+                t_error.Start(3);
+                return;
+            }
+            var scr = new StreamReader("Assets/Scripts/RaymapGame/PersoEditor/NewPersoScript.txt");
+            var outs = scr.ReadToEnd()
+                .Replace("Author", author)
+                .Replace("NewPersoScript", persoNameOrModelOrFamily)
+                .Replace("Description", englishName).Split(new string[] { "~~~" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            var newScr = new StreamWriter(newPath);
+            if (author != "") newScr.Write(outs[0]);
+            newScr.Write(outs[1]);
+
+            scr.Close();
+            newScr.Close();
+        }
+
+
+        static Timer t_error = new Timer();
+        static float nameWidth = 160;
+
+        public static PersoBehaviour GetSelectedPersoBehaviour() {
+            return Selection.activeGameObject.GetComponentInParent<PersoBehaviour>();
+        }
+
+
+        void Header(string text) {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(text, EditorStyles.toolbarButton);
+            GUILayout.EndHorizontal();
+            GUILayout.Space(3);
+        }
+
+        Timer t_scan = new Timer();
+        static PersoController[] persos = new PersoController[0];
+        public void RescanLevel() {
+            persos = FindObjectsOfType<PersoController>();
+        }
+
+        void FixedUpdate() {
+            t_scan.Start(1.5f, RescanLevel, false);
+        }
+
+
+        static string englishName = "";
+
+        public void OnGUI() {
+            Header("Create Perso Script");
+
+            Main.GetPersoScripts();
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("English name:", GUILayout.Width(nameWidth));
+            englishName = GUILayout.TextField(englishName);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Create from Selected Perso", GUILayout.Width(nameWidth));
+            float bw = 50;
+            if (GUILayout.Button("Name"))
+                CreateNewPersoScript(GetSelectedPersoBehaviour().perso.namePerso, englishName);
+            if (GUILayout.Button("Model"))
+                CreateNewPersoScript(GetSelectedPersoBehaviour().perso.nameModel, englishName);
+            if (GUILayout.Button("Family"))
+                CreateNewPersoScript(GetSelectedPersoBehaviour().perso.nameFamily, englishName);
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(nameWidth);
+            if (t_error.active) {
+                GUILayout.Label("Perso script with this name already exists");
+            }
+            GUILayout.EndHorizontal();
+
+            Header("Perso Scripts");
+
+            foreach (var s in Main.persoScripts) {
+                EditorGUILayout.BeginToggleGroup(s.Name, true);
+                foreach (var p in persos) {
+                    if (p.GetType() == s) {
+                        EditorGUILayout.ObjectField(p, typeof(PersoController), true);
+                    }
+                }
+                EditorGUILayout.EndToggleGroup();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/RaymapGame/PersoEditorWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RaymapGame.PersoEditor {
+    public class PersoEditorWindow : EditorWindow {
+
+        [MenuItem("Raymap/RaymapGame/Perso Editor")]
+        public static void ShowWindow() {
+            GetWindow(typeof(PersoEditorWindow));
+        }
+
+
+        public MethodBody body = new MethodBody(/*
+            new MethodInvoke(TypeData.loaded.actions["SetFriction"]),
+            new MethodInvoke(TypeData.loaded.actions["SetRule"])
+            */);
+
+        void OnGUI() {
+            foreach (var a in body.body) {
+                a.Inspect(EditorMode.UnityInspector);
+            }
+        }
+    }
+}
